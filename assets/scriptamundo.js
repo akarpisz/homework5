6b1d56a6ba2c6ecd0d9225f9163483fc82fd7810
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-04-28 15:44:51 -0600

    minor refactoring

--- assets/scriptamundo.js
+++ assets/scriptamundo.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-  //VARIABLES 
+  //VARIABLES
   var currentDayEl = $("#currentDay");
   //using the moment.js library, variable is set to the current time and date
   var rightNow = moment();
+
+  var row;
+  var cell1;
+  var cell2;
+  var cell3;
+  var textbox;
+  var button;
 
   //updates the current day element every second, with the current time and date
   setInterval(function () {
@@ -10,24 +17,33 @@ $(document).ready(function () {
     currentDayEl.text(now);
   }, 1000);
 
+  //defining the function that will color the cells based on the current time
+  function colorCells(modSM) {
+    if (modSM.format("h") === rightNow.format("h")) {
+      $(cell2).attr("style", "background-color:red");
+    } else if (modSM.isBefore(rightNow)) {
+      $(cell2).attr("style", "background-color:gray");
+    } else if (modSM.isAfter(rightNow)) {
+      $(cell2).attr("style", "background-color:green");
+    }
+  }
+
   //function creating the table
   function renderTB() {
     //selecting the table element based on its class
     var table = $(".blocks");
-    //while loop incrementor
-    var i = 0;
     //moment.js "moment", modified to the start time of the planner
     var modSM = moment().hours(7).minutes(0).seconds(0).milliseconds(0);
 
-    while (i < 13) {
+    for (var i = 0;i < 12; i++) {
       //new elements to be created in each row
-      var row = $("<tr>");
-      var cell1 = $("<td>");
-      var cell2 = $("<td>");
-      var cell3 = $("<td>");
-      var textbox = $("<textarea/>");
-      var button = $("<button>");
-      
+      row = $("<tr>");
+      cell1 = $("<td>");
+      cell2 = $("<td>");
+      cell3 = $("<td>");
+      textbox = $("<textarea/>");
+      button = $("<button>");
+
       var cellId = i;
       //extracting the modified moment's current hour and am/pm indicatino
       var currH = modSM.format("h");
@@ -38,13 +54,7 @@ $(document).ready(function () {
       var data = JSON.parse(localStorage.getItem(cellId));
 
       //this colors the blocks based on whether they are before or after the current hour,  or within the current hour.
-      if (modSM.format("h") === rightNow.format("h")) {
-        $(cell2).attr("style", "background-color:red");
-      } else if (modSM.isBefore(rightNow)) {
-        $(cell2).attr("style", "background-color:gray");
-      } else if (modSM.isAfter(rightNow)) {
-        $(cell2).attr("style", "background-color:green");
-      }
+      colorCells(modSM);
       //adds the events from local storage to the textbox, if any entries exist for that timeblock's id
       textbox.text(data);
       //appendss the modified time to cell1
@@ -54,7 +64,6 @@ $(document).ready(function () {
 
       //assigns cell2 its id
       $(cell2).attr("id", i);
-      
 
       //assigns the same id to the button
       $(button).attr("id", i);
@@ -64,10 +73,8 @@ $(document).ready(function () {
       cell3.append(button);
       //append all 3 cells to the row, which are appended to the table
       table.append(row.append(cell1, cell2, cell3));
-      //increment the time
+      //increment the modified start time
       modSM.add("1", "h");
-      //increment i
-      i++;
     }
   }
   //call render table function
@@ -80,7 +87,9 @@ $(document).ready(function () {
     //variable storing the id from the button that was clicked
     var id = $(clicked).parent().prev().attr("id");
     //traversing the table, to the previous cell (td), and storing the stringified value of its child element, the text area element.
-    var prevBoxText = JSON.stringify($(clicked).parent().prev().children().val());
+    var prevBoxText = JSON.stringify(
+      $(clicked).parent().prev().children().val()
+    );
     //based on the buttons id, the contents of the neighboring textbox are stored in the browser's local storage
     localStorage.setItem(id, prevBoxText);
     //the table is re-rendered with the new stored values included
